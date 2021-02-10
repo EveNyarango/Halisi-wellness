f6e86e1d4a9e996c8c822ade95b914c27d254dce
EveNyarango <evenyarango@gmail.com>
2021-02-10 15:55:01 +0300

    add activities

---
+++ app/src/main/java/com/example/halisiwellness/ui/ChatboxActivity.java
@@ -0,0 +1,16 @@
+package com.example.halisiwellness.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.halisiwellness.R;
+
+public class ChatboxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatbox);
+    }
+}
