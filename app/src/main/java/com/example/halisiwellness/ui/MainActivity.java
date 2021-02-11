0ea96f02c6c958f3a4ec418d2a998b99a0426039
EveNyarango <evenyarango@gmail.com>
2021-02-11 15:43:08 +0300

    add butterknife

--- app/src/main/java/com/example/halisiwellness/ui/MainActivity.java
+++ app/src/main/java/com/example/halisiwellness/ui/MainActivity.java
@@ -3,10 +3,21 @@ package com.example.halisiwellness.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.halisiwellness.R;
 
+import butterknife.BindView;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.ProfileImageView)
+    ImageView mProfileImageView;
+    @BindView(R.id.AppointImageView) ImageView mAppointImageView;
+    @BindView(R.id.BlogImageView) ImageView mBlogImageView;
+    @BindView(R.id.CalImageView) ImageView mCalImageView;
+    @BindView(R.id.DiaryImageView) ImageView mDiaryImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
