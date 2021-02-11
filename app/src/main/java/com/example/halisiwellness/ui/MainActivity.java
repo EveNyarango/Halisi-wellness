36e92e4084fd62e23513b227ab6c56985e1e2cce
EveNyarango <evenyarango@gmail.com>
2021-02-11 23:15:27 +0300

    changes on the ui

--- app/src/main/java/com/example/halisiwellness/ui/MainActivity.java
+++ app/src/main/java/com/example/halisiwellness/ui/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.halisiwellness.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.example.halisiwellness.R;
 
@@ -28,10 +30,34 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         ButterKnife.bind(this);
 
         mProfileImageView.setOnClickListener(this);
+        mAppointImageView.setOnClickListener(this);
+        mBlogImageView.setOnClickListener(this);
+        mCalImageView.setOnClickListener(this);
+        mDiaryImageView.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
 
+        if(v == mProfileImageView) {
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        }
+        if(v == mAppointImageView) {
+            Intent intent = new Intent(MainActivity.this, AppointmentActivity.class);
+            startActivity(intent);
+        }
+        if(v == mBlogImageView) {
+            Intent intent = new Intent(MainActivity.this, ArticlesActivity.class);
+            startActivity(intent);
+        }
+        if(v == mCalImageView) {
+            Intent intent = new Intent(MainActivity.this, ReminderActivity.class);
+            startActivity(intent);
+        }
+        if(v == mDiaryImageView) {
+            Intent intent = new Intent(MainActivity.this, DiaryActivity.class);
+            startActivity(intent);
+        }
     }
 }
