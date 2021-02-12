e6fadbda23e1062dab25c1aca5bae3aa4b24b968
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-02-12 07:17:52 +0300

    added functionality for getting otp code from firebase

--- app/src/main/java/com/example/halisiwellness/ui/MainActivity.java
+++ app/src/main/java/com/example/halisiwellness/ui/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.halisiwellness.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.halisiwellness.R;
+
+import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Intent intent = new Intent( MainActivity.this,OtpActivity.class);
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep( 5000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };thread.start();
     }
 }
