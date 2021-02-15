309aa8cc64e5576169863da02cc579e6d1456310
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-02-15 13:14:38 +0300

    create ui for meal plan

--- app/src/main/java/com/example/halisiwellness/ui/LoginActivity.java
+++ app/src/main/java/com/example/halisiwellness/ui/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.halisiwellness.R;
 
@@ -14,8 +15,8 @@ import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @BindView(R.id.login_btn)
-    Button mLogonBtn;
+    @BindView(R.id.login_btn) Button mLogonBtn;
+    @BindView(R.id.textRegister) TextView mTextRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,14 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         mLogonBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mTextRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
