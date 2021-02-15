3d96bcdbea470c9b71121e8be8b831fc7a98042d
EveNyarango <evenyarango@gmail.com>
2021-02-16 02:44:56 +0300

    time to sleep

--- app/src/main/java/com/example/halisiwellness/ui/CalenderActivity.java
+++ app/src/main/java/com/example/halisiwellness/ui/CalenderActivity.java
@@ -1,16 +1,32 @@
 package com.example.halisiwellness.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.example.halisiwellness.R;
 
 public class CalenderActivity extends AppCompatActivity {
-
+CalendarView calendarView;
+TextView myDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
+
+        calendarView= (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.myDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int i, int i1, int i2) {
+                String date = (i1 + 1) + "/" + 12 + "/" + i;
+                myDate.setText(date);
+
+            }
+        });
     }
 }
