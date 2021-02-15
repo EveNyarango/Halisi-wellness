e4644cb56a8035223698e4881ed18a2e3da699e4
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-02-15 06:29:15 +0300

    create ui for otp activity

--- app/src/main/java/com/example/halisiwellness/ui/LoginActivity.java
+++ app/src/main/java/com/example/halisiwellness/ui/LoginActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import com.example.halisiwellness.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,11 +21,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
         mLogonBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
             }
         });
     }
