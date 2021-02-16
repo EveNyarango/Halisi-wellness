bdb2b14a73ceb54663f2da0ed036e5ea328cc1b6
EveNyarango <evenyarango@gmail.com>
2021-02-16 16:39:19 +0300

    add date picker to profile

--- app/src/main/java/com/example/halisiwellness/ui/ProfileActivity.java
+++ app/src/main/java/com/example/halisiwellness/ui/ProfileActivity.java
@@ -2,15 +2,61 @@ package com.example.halisiwellness.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.halisiwellness.R;
 
+import java.util.Calendar;
+
 public class ProfileActivity extends AppCompatActivity {
+    private static final  String TAG = "ProfileActivity";
+    private EditText mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mDisplayDate = (EditText) findViewById(R.id.Birth2);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal .get(Calendar.YEAR);
+                int month = cal .get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        ProfileActivity.this,
+                        android.R.style.Theme_Holo_Dialog_NoActionBar_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+
+            }
+        };
     }
 }
