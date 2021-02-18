430f32b69a98c22e50e1358ffd2279562f2ba87e
EveNyarango <evenyarango@gmail.com>
2021-02-18 03:16:04 +0300

    successful shared preferences

--- app/src/main/java/com/example/halisiwellness/ui/DiaryActivity.java
+++ app/src/main/java/com/example/halisiwellness/ui/DiaryActivity.java
@@ -14,13 +14,13 @@ import com.example.halisiwellness.R;
 
 public class DiaryActivity extends AppCompatActivity {
 
-    EditText breakfast, lunchy, snacky, supper;
-    Button btnDiary;
+    EditText editText_breakfast, editText_lunchy, editText_snacky, editText_supper;
+    Button button_btnDiary;
 
     SharedPreferences sharedPreferences;
 
     private static final String SHARED_PREF_NAME = "mypref";
-    private static final String KEY_BREAK = "break";
+    private static final String KEY_BREAKY = "breaky";
     private static final String KEY_LUNCH = "lunch";
     private static final String KEY_SNACK = "snack";
     private static final String KEY_SAPA = "sapa";
@@ -31,27 +31,34 @@ public class DiaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
 
-        breakfast = findViewById(R.id.breakfast);
-        lunchy = findViewById(R.id.lunchy);
-        snacky = findViewById(R.id.snacky);
-        supper = findViewById(R.id.supper);
-        btnDiary = findViewById(R.id.btnDiary);
+        editText_breakfast = findViewById(R.id.editText_breakfast);
+     editText_lunchy = findViewById(R.id.editText_lunchy);
+        editText_snacky = findViewById(R.id.editText_snacky);
+        editText_supper = findViewById(R.id.editText_supper);
+        button_btnDiary = findViewById(R.id.button_btnDiary);
 
-        sharedPreferences = getSharedPreferences("", MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
 
-        btnDiary.setOnClickListener(new View.OnClickListener() {
+        String breaky = sharedPreferences.getString(KEY_BREAKY, null);
+
+        if(breaky !=null){
+            Intent intent = new Intent(DiaryActivity.this, Diary2Activity.class);
+            startActivity(intent);
+        }
+
+        button_btnDiary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString(KEY_BREAK, breakfast.getText().toString());
-                editor.putString(KEY_LUNCH, lunchy.getText().toString());
-                editor.putString(KEY_SNACK, snacky.getText().toString());
-                editor.putString(KEY_SAPA, supper.getText().toString());
+                editor.putString(KEY_BREAKY, editText_breakfast.getText().toString());
+                editor.putString(KEY_LUNCH, editText_lunchy.getText().toString());
+                editor.putString(KEY_SNACK, editText_snacky.getText().toString());
+                editor.putString(KEY_SAPA, editText_supper.getText().toString());
                 editor.apply();
                 Intent intent = new Intent(DiaryActivity.this, Diary2Activity.class);
                 startActivity(intent);
 
-                Toast.makeText(DiaryActivity.this, "Diary updated successfully", Toast.LENGTH_SHORT).show();
+                Toast.makeText(DiaryActivity.this, "Food Diary updated successfully", Toast.LENGTH_SHORT).show();
             }
         });
     }
