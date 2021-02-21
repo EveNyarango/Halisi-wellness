0197545f0f55a7a5ff00cc4ca4cecf739049e545
EveNyarango <evenyarango@gmail.com>
2021-02-21 22:34:56 +0300

    implement camera

---
+++ app/src/main/java/viewholder/LineViewHolder.java
@@ -0,0 +1,12 @@
+package viewholder;
+
+//public class LineViewHolder RecyclerView.ViewHolder {
+//
+//    public LineViewHolder mTimelineView;
+//
+//    public TimeLineViewHolder(View itemView, int viewType) {
+//        super(itemView);
+//        mTimelineView = (TimelineView) itemView.findViewById(R.id.timeline);
+//        mTimelineView.initLine(viewType);
+//    }
+//}
