72561ee8c6b2bed401aeb658d9030259ec3b5849
EveNyarango <evenyarango@gmail.com>
2021-02-25 12:07:53 +0300

    changes

--- app/src/main/java/com/example/halisiwellness/adapter/MealsPlanAdapter.java
+++ app/src/main/java/com/example/halisiwellness/adapter/MealsPlanAdapter.java
@@ -6,22 +6,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import com.example.halisiwellness.R;
-
 import viewholder.MealsPLanViewHolder;
-
 public class MealsPlanAdapter extends ArrayAdapter<String> {
     Context mContext;
     int[] mImages;
     String[] mMealsName;
     String[] mMealsDays;
     String[] mMealsDescription;
-
-//    public HighFibreAdapter(Context mContext, int[] mImages, String[] mMealsName, String[] mMealsDays, String[] mMealsDescription) {
+    //    public HighFibreAdapter(Context mContext, int[] mImages, String[] mMealsName, String[] mMealsDays, String[] mMealsDescription) {
 //        super(mContext, R.layout.meal_item,R.id.meals,mMealsName);
 //        this.mContext = mContext;
 //        this.mImages = mImages;
@@ -30,7 +25,6 @@ public class MealsPlanAdapter extends ArrayAdapter<String> {
 //        this.mMealsName = mMealsName;
 //
 //    }
-
     public MealsPlanAdapter(Context mContext, String[] mMealsName, String[] mMealsDescription, int[] mMealsImages, String[] mMealsDays) {
         super(mContext, R.layout.meal_item,R.id.meals,mMealsName);
         this.mContext = mContext;
@@ -39,25 +33,19 @@ public class MealsPlanAdapter extends ArrayAdapter<String> {
         this.mMealsDays = mMealsDays;
         this.mImages = mMealsImages;
     }
-
-
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
         View singleItem = convertView;
-
         MealsPLanViewHolder holder = null;
         if (singleItem == null) {
             LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             singleItem = layoutInflater.inflate(R.layout.meal_item, parent, false);
-            holder = new MealsPLanViewHolder(singleItem);
+            holder = new MealsPLanViewHolder(singleItem,0);
             singleItem.setTag(holder);
         }
-
         else {
             holder = (MealsPLanViewHolder) singleItem.getTag();
         }
-
         holder.mealImage.setImageResource(mImages[position]);
         holder.mMealName.setText(mMealsName[position]);
         holder.mMealDescription.setText(mMealsDescription[position]);
@@ -71,3 +59,8 @@ public class MealsPlanAdapter extends ArrayAdapter<String> {
         return singleItem;
     }
 }
+
+
+
+
+
--- app/src/main/java/viewholder/MealsPLanViewHolder.java
+++ app/src/main/java/viewholder/MealsPLanViewHolder.java
@@ -3,21 +3,49 @@ package viewholder;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import androidx.recyclerview.widget.RecyclerView;
 import com.example.halisiwellness.R;
-
-public class MealsPLanViewHolder {
-
+import com.github.vipulasri.timelineview.TimelineView;
+public class MealsPLanViewHolder extends RecyclerView.ViewHolder {
     public ImageView mealImage;
     public TextView mMealName;
     public TextView mMealDay;
     public TextView mMealDescription;
-
-    public MealsPLanViewHolder(View view){
-
+    public TimelineView mTimelineView;
+    public MealsPLanViewHolder(View view, int ViewType){
+        super(view);
         mealImage = view.findViewById(R.id.MealImageView);
         mMealDescription =view.findViewById(R.id.Description);
         mMealDay = view.findViewById(R.id.Days);
         mMealName = view.findViewById(R.id.meals);
+        // TimeLine
+        mTimelineView = (TimelineView) itemView.findViewById(R.id.timeline);
+        mTimelineView.initLine(ViewType);
     }
+//    @Override
+//    public int getItemViewType(int position) {
+//        return TimelineView.getTimeLineViewType(position, getItemCount());
+//    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
