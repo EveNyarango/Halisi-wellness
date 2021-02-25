cf07273ee1b8ca32b755679f3e3c9c78ef355ba0
Joseph-beep-tech <Josephcluff006@gmail.com>
2021-02-26 01:56:11 +0300

    article changes

--- app/src/main/java/com/example/halisiwellness/Constants.java
+++ app/src/main/java/com/example/halisiwellness/Constants.java
@@ -4,7 +4,7 @@ package com.example.halisiwellness;
 
 public class Constants {
     public static final String HALISI_BASE_URL = "https://young-tundra-50012.herokuapp.com/";
-//    public static final String HALISI_TOKEN = BuildConfig.HALISI_TOKEN;
+
 
 
 }
