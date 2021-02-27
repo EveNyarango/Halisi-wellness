3d875cee1a6aa59c592bebf9b418e8e614525eb3
EveNyarango <evenyarango@gmail.com>
2021-02-28 00:54:04 +0300

    redesign food diary

--- app/src/main/java/com/example/halisiwellness/ui/LineActivity.java
+++ app/src/main/java/com/example/halisiwellness/ui/LineActivity.java
@@ -3,10 +3,14 @@ package com.example.halisiwellness.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.halisiwellness.R;
 
 public class LineActivity extends AppCompatActivity {
+
+
+    Button button_btnDiary2, button_btnDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
